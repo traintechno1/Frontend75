e5b0aee3c22b8c85cb1e19e1d785f7f33ecdd343
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2024-09-19 06:57:10 +0530

    CSS Flex, alignment, JS Started

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+//variable declaration (here we have declared variable with name val)
+var val;
+console.log(val);
+
+// assignment
+val = 100;
+console.log(val);
+
+val = "Viraj";
+console.log(val);
+
+// variable declaration with assignment
+var val1 = 10;
+
+// console.log(val);
+console.log(val1);
+
+console.log("Hello From JS");
+console.log("This is my first JS message in console");
