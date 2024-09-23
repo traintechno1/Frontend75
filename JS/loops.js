726c37a2fdf64e55f3960ba3b02bedd976111191
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2024-09-23 07:22:22 +0530

    Loops in JS

---
+++ JS/loops.js
@@ -0,0 +1,19 @@
+
+// for(intialize, condition, increment/decrement){}
+// for(let i = 1; i <= 10; i++){
+//     console.log(9 * i);
+// }
+
+let i = 1;
+while(i<=10){
+    console.log(9 * i);
+    i++;
+}
+
+console.log("------------In Do While------------");
+let j = 0;
+do{
+    console.log(j);
+    j++
+    // increment/decrement
+}while(j<=10);
