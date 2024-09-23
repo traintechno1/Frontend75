b75643fc4fba6f12e7f85a7bcfb30df4494e93d1
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2024-09-23 07:40:55 +0530

    Basics of array

---
+++ JS/array.js
@@ -0,0 +1,20 @@
+
+// let val1 = 10;
+// let val2 = 10;
+// let val3 = 10;
+// let val4 = 10;
+// let val5 = 10;
+// let val6 = 10;
+// let val7 = 10;
+
+let arr = [10,20,30,40,50,60]; // array
+console.log(arr);
+
+arr[0] = 564;
+arr[1] = "Gurveer";
+arr[2] = true;
+// arr[3] = [1,2,42,24,24,536,3434];
+for(let i=0; i< arr.length; i++){
+    console.log(arr[i]); //accessing specific index of array
+}
+// console.log(arr[3][1]);
