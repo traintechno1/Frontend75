0f0b8619fc00e24f935c456ee12a3a2716a634f3
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2024-09-25 07:53:50 +0530

    Array Methods in JS

--- JS/array.js
+++ JS/array.js
@@ -6,15 +6,71 @@
 // let val5 = 10;
 // let val6 = 10;
 // let val7 = 10;
+// [1,2,3,4,5, [700, 800]],
+let arr = [10,20,30,40,50,61, 100, 200]; // array
+console.log('original array', arr);
 
-let arr = [10,20,30,40,50,60]; // array
-console.log(arr);
-
-arr[0] = 564;
-arr[1] = "Gurveer";
-arr[2] = true;
+// arr[0] = 564;
+// arr[1] = "Gurveer";
+// arr[2] = true;
 // arr[3] = [1,2,42,24,24,536,3434];
-for(let i=0; i< arr.length; i++){
-    console.log(arr[i]); //accessing specific index of array
-}
+// for(let i=0; i< arr.length; i++){
+//     console.log(arr[i]); //accessing specific index of array
+// }
 // console.log(arr[3][1]);
+// let arr2 = [100, 200, 300, 400];
+
+// arr = arr.concat([100, 200, 300, 400]);
+
+// arr = arr.copyWithin(3, 0, 3);
+
+// console.log(arr.every((e)=>e % 2 === 0))
+
+// console.log(arr.fill(53, 0, 3));
+
+// console.log(arr.filter((e)=> e > 30 ));
+
+// console.log(arr.find(e=>e === 41))
+
+// console.log(arr.findIndex(e=>e == 41));
+
+// console.log(arr.flat(2));
+
+// arr.forEach((e)=>{
+//     console.log(e + 100);
+// })
+
+// console.log(arr.includes(40));
+
+// console.log(arr.indexOf(30));
+
+// console.log(arr.join('-'));
+
+// let products = ['Rice', 'Bread', 'Eggs'];
+
+// console.log(products.join(', '));
+
+// console.log(arr.map(e=> e * 3));
+// arr.push(87);
+// arr.push(78);
+
+// console.log('poped : ', arr.pop());
+// console.log('poped : ', arr.pop());
+
+// console.log(arr.reduce((sum,e)=> sum + e , 0));
+
+// console.log(arr.reverse());
+
+// console.log(arr.shift());
+
+// console.log(arr.slice(4, 6));
+
+// console.log(arr.some(e => e > 199));
+
+// console.log(arr.sort((a,b)=> b - a));
+
+// console.log(arr.splice(0, 4));
+
+// arr.unshift(11);
+
+console.log(arr);
